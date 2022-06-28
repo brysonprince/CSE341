ab1bd4da8fad348e4565339bade97aeaac17844e
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-06-28 16:03:46 -0700

    Heroku bug fixing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app
     .use(bodyParser.json())
