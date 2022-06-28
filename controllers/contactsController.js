8e0ac446b8584b26acb3f1af2d25d97b2fd265ce
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-06-28 11:06:10 -0700

    Personal assignment 2 finished

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,57 +1,5 @@
-/*'use strict';
-
-
-const mongoose = require('mongoose'),
-  Contact = mongoose.model('Contact');
-
-exports.list_all_contacts = function (req, res) {
-  Contact.find({}, function (err, contacts) {
-    if (err)
-      res.send(err);
-    res.json(contacts);
-  });
-};
-
-
-exports.create_a_contact = function (req, res) {
-  var new_contact = new Contact(req.body);
-  new_contact.save(function (err, contact) {
-    if (err)
-      res.send(err);
-    res.json(contact);
-  });
-};
-
-
-exports.read_a_contact = function (req, res) {
-  Contact.findById(req.params.taskId, function (err, contact) {
-    if (err)
-      res.send(err);
-    res.json(contact);
-  });
-};
-
-
-exports.update_a_contact = function (req, res) {
-  Contact.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, function (err, contact) {
-    if (err)
-      res.send(err);
-    res.json(contact);
-  });
-};
-
-
-exports.delete_a_contact = function (req, res) {
-  Task.remove({
-    _id: req.params.contactId
-  }, function (err, contact) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Contact successfully deleted' });
-  });
-};*/
-
 const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
@@ -61,4 +9,16 @@ const getAll = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll };
+const getItem = async (req, res) => {
+  const itemId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('contacts').find({ _id: itemId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+}
+
+module.exports = { 
+  getAll,
+  getItem
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -3,5 +3,6 @@ const routes = require('express').Router();
 const contactsController = require('../controllers/contactsController');
 
 routes.get('/', contactsController.getAll);
+routes.get('/:id', contactsController.getItem);
 
 module.exports = routes;
