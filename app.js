016e747b91b2cbedd7165622d39932280ac6286f
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-07-02 11:48:40 -0700

    debug

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ app
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-        /*res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');*/
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     })
     .use('/', require('./routes'));
