9b462936ea6790dfbba4cccf04021e4361de741e
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-07-02 16:22:50 -0700

    debug

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -33,6 +33,7 @@ const getContact = async (req, res) => {
 const createContact = async (req, res) => {
   let contact = buildContact(req);
   const result = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  contact._id = result.insertedId;
   if(result.acknowledged) {
     res.status(201).json(result);
   }
