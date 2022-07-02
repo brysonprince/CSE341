eafda8cc6cb7ea4bd3356a835ffc6b7e0dda6f39
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-07-01 21:51:26 -0700

    Contacts final touches

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app
     .use(bodyParser.json())
     .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
